fdb2ce748a3d1bf82d8b2c5dc1ae3c14b64f401c
Pedrooaj <pedroantonio5735@gmail.com>
2024-09-23 12:27:12 -0300

    dev branch

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-import App from "./app";
+import express from 'express';
+import { Request, Response } from 'express';
 
 
-App();
+const app: express.Application = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hello World");
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000");
+});
+
