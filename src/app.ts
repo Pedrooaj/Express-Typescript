3142973d83e6eea40f7fbf4ff9eef0fb32f41208
Pedrooaj <pedroantonio5735@gmail.com>
2024-09-23 11:49:01 -0300

    remove feat

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,6 @@ import express from 'express';
 const App = () => {
     const app: express.Application = express();
 
-
-    app.get('/', (req, res) => {
-        res.send('Hello World');
-    })
-
     app.listen(3000, () => {
         console.log("Servidor rodando na porta 3000");
     });
