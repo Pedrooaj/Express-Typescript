0323aa8ee33571a98587f97ccecf611e062ee7ff
Pedrooaj <pedroantonio5735@gmail.com>
2024-09-23 18:13:36 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 import express from 'express';
+import { Request, Response } from "express"
 const port: number = 3000;
-import { Request, Response } from 'express';
 const app: express.Application = express();
 
 app.get('/', (req: Request, res: Response) => {
     res.send("Hello World");
 })
 
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+app.get('/consulta/',(req: Request,res: Response) => {
+    // ( req ) Request --> Dados enviados pelo cliente
+    let cpf = req.query['cpf'];
+    
+    // ( res ) Response --> Dados enviados para o cliente
+    res.send("Retorno consula: CPF = " + cpf);
+});
+
+
+// lembrando de usar err?: pois o erro pode ou não vir
+app.listen(port, function(err?: Error){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(`Servidor ligado na porta ${port}`);
+    }
 });
 
